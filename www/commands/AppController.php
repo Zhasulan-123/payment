78c69f26a76ec142344c27cc230821e9ba5feafe
Zhasulan-123 <zhas-88@bk.ru>
2020-09-04 11:54:33 +0600

    command

---
+++ www/commands/AppController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\commands;
+
+use app\models\User;
+use Yii;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class AppController extends Controller{
+
+   public function actionAddUser($email, $password, $username, $fullname)
+   {
+        $security = Yii::$app->security;
+
+        $user = new User();
+        $user->email = $email;
+        $user->password_hash = $security->generatePasswordHash($password);
+        $user->username = $username;
+        $user->fullname = $fullname;
+        $user->auth_key = $security->generateRandomString(255);
+        if ($user->save()) {
+            Console::output('saved');
+        } else {
+            var_dump($user->errors);
+            Console::output("not saved");
+        }
+   }
+
+}
